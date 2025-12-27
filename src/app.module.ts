1931c4804ed9810f2120880688e549ed08f201c9
jeferson <jeferson.gomes@eteg.com.br>
2025-12-27 11:54:53 -0300

    feat(auth): add global module decorator and clean up import paths in AuthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module.js";
 import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
 import { APP_GUARD } from "@nestjs/core";
 import { AuthGuard } from "./modules/auth/auth.guard.js";
 
+@Global()
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { LoginUseCase } from "../use-cases/login.use-case.js";
-import { RegisterUserUseCase } from "../use-cases/register-user.use-case.js";
-import { LoginDto } from "../dto/login.dto.js";
-import { RegisterDto } from "../dto/register.dto.js";
+import { LoginUseCase } from "../use-cases/login.use-case";
+import { RegisterUserUseCase } from "../use-cases/register-user.use-case";
+import { LoginDto } from "../dto/login.dto";
+import { RegisterDto } from "../dto/register.dto";
+import { IsPublic } from "src/shared/decorators/isPublic";
 
 @Controller("auth")
 export class AuthController {
@@ -11,13 +12,16 @@ export class AuthController {
     private readonly registerUseCase: RegisterUserUseCase,
   ) {}
 
+  @IsPublic()
   @Post("login")
   @HttpCode(HttpStatus.OK)
   async login(@Body() body: LoginDto) {
     return this.loginUseCase.execute(body);
   }
 
+  @IsPublic()
   @Post("register")
+  @HttpCode(HttpStatus.CREATED)
   async register(@Body() body: RegisterDto) {
     return this.registerUseCase.execute(body);
   }
